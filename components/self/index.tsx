e68d46e387363d0b36a57a11d54375ffae42c5f5
Maxwell Dunk-Green <maxwellmdg@gmail.com>
2026-03-14 13:58:49 -0500

    update selfie

--- components/self/index.tsx
+++ components/self/index.tsx
@@ -1,22 +1,17 @@
-const style = {
-    height: '100%',
-    width: '100%',
-};
-
 
 export default function Self() {
 
 
     return (
         <div
-            className="bg-transparentContainer h-[125px] xxs:h-[150px] lg:h-[300px]"
+            className="bg-transparentContainer h-[125px] xxs:h-[150px] lg:h-[335px]"
             style={{
                 aspectRatio: '0.833',
                 border: '4px outset rgba(0,0,0,0.3)',
             }}
         >
             {/* {pic} */}
-            <img src="/images/Self_Autumn.png" style={style} />
+            <img src="/images/Self_Autumn_2.png" style={{ width: '100%', height: '100%', objectFit: 'cover', transform: 'scale(1.23)' }} />
         </div>
     );
 }
